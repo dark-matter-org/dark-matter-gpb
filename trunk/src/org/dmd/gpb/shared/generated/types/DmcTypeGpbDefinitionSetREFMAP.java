af8c5eee098d90c47e40c4507e925c6fb9c4adad
pstrong99 <pstrong99@f9b6f35e-91de-e0b8-5825-bcd424a19d09>
2013-01-21 00:28:09 +0000

    Fleshing out the object model.

---
+++ trunk/src/org/dmd/gpb/shared/generated/types/DmcTypeGpbDefinitionSetREFMAP.java
@@ -0,0 +1,220 @@
+//	---------------------------------------------------------------------------
+//	dark-matter-data
+//	Copyright (c) 2011 dark-matter-data committers
+//	---------------------------------------------------------------------------
+//	This program is free software; you can redistribute it and/or modify it
+//	under the terms of the GNU Lesser General Public License as published by the
+//	Free Software Foundation; either version 3 of the License, or (at your
+//	option) any later version.
+//	This program is distributed in the hope that it will be useful, but WITHOUT
+//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+//	more details.
+//	You should have received a copy of the GNU Lesser General Public License along
+//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	---------------------------------------------------------------------------
+package org.dmd.gpb.shared.generated.types;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.Iterator;
+import org.dmd.dmc.DmcAttribute;
+import org.dmd.dmc.DmcAttributeInfo;
+import org.dmd.dmc.DmcValueException;
+import org.dmd.dmc.DmcMappedAttributeIF;
+import org.dmd.dms.generated.enums.ValueTypeEnum;
+import org.dmd.dmc.types.CamelCaseName;    // key type import
+/**
+ * The DmcTypeGpbDefinitionSetREFMAP provides storage for a map of GpbDefinitionSetREF
+ * <P>
+ * This code was auto-generated and shouldn't be altered manually!
+ * Generated from: org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2828)
+ *    Called from: org.dmd.dms.util.DmoTypeFormatter.dumpNamedREF(DmoTypeFormatter.java:540)
+ */
+@SuppressWarnings("serial")
+// public class DmcTypeGpbDefinitionSetREFMAP extends DmcTypeGpbDefinitionSetREF<GpbDefinitionSetREF,CamelCaseName> {
+public class DmcTypeGpbDefinitionSetREFMAP extends DmcTypeGpbDefinitionSetREF implements Serializable {
+    
+    private final static Iterator<GpbDefinitionSetREF> emptyList = (new HashMap<CamelCaseName,GpbDefinitionSetREF>()).values().iterator();
+    
+    protected Map<CamelCaseName,GpbDefinitionSetREF> value;
+    
+    public DmcTypeGpbDefinitionSetREFMAP(){
+        value = null;
+    }
+    
+    public DmcTypeGpbDefinitionSetREFMAP(DmcAttributeInfo ai){
+        super(ai);
+        initValue();
+    }
+    
+    void initValue(){
+        if (getAttributeInfo().valueType == ValueTypeEnum.HASHMAPPED)
+            value = new HashMap<CamelCaseName,GpbDefinitionSetREF>();
+        else
+            value = new TreeMap<CamelCaseName,GpbDefinitionSetREF>();
+    }
+    
+    public CamelCaseName firstKey(){
+        if (getAttributeInfo().valueType == ValueTypeEnum.TREEMAPPED){
+            if (value == null)
+                return(null);
+            TreeMap<CamelCaseName,GpbDefinitionSetREF> map = (TreeMap<CamelCaseName,GpbDefinitionSetREF>)value;
+            return(map.firstKey());
+        }
+        throw(new IllegalStateException("Attribute " + getAttributeInfo().name + " is HASHMAPPED and doesn't support firstKey()"));
+    }
+    
+    @Override
+    public DmcTypeGpbDefinitionSetREFMAP getNew(){
+        return(new DmcTypeGpbDefinitionSetREFMAP(getAttributeInfo()));
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2885)
+    public DmcAttribute<GpbDefinitionSetREF> cloneIt(){
+        synchronized(this){
+            DmcTypeGpbDefinitionSetREFMAP rc = getNew();
+    
+            if (value == null)
+                return(rc);
+    
+            for(GpbDefinitionSetREF val: value.values())
+            try {
+                rc.add(val);
+            } catch (DmcValueException e) {
+                throw(new IllegalStateException("typeCheck() should never fail here!",e));
+            }
+            return(rc);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2905)
+    public GpbDefinitionSetREF add(Object v) throws DmcValueException {
+        synchronized(this){
+            GpbDefinitionSetREF newval = typeCheck(v);
+            if (value == null)
+                initValue();
+            CamelCaseName key = (CamelCaseName)((DmcMappedAttributeIF)newval).getKey();
+            GpbDefinitionSetREF oldval = value.put(key,newval);
+            
+            if (oldval != null){
+                // We had a value with this key, ensure that the value actually changed
+                if (oldval.valuesAreEqual(newval))
+                    newval = null;
+            }
+            
+            return(newval);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2926)
+    public GpbDefinitionSetREF del(Object key){
+        synchronized(this){
+    
+            if (value == null)
+                return(null);
+    
+           if (key instanceof CamelCaseName)
+                return(value.remove(key));
+            else
+                throw(new IllegalStateException("Incompatible key type: " + key.getClass().getName() + " passed to del():" + getName()));
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2942)
+    public Iterator<GpbDefinitionSetREF> getMV(){
+        synchronized(this){
+    
+            if (value == null)
+                return(emptyList);
+    
+            Map<CamelCaseName,GpbDefinitionSetREF> clone = null;
+            if (getAttributeInfo().valueType == ValueTypeEnum.HASHMAPPED)
+                clone = new HashMap<CamelCaseName,GpbDefinitionSetREF>(value);
+            else
+                clone = new TreeMap<CamelCaseName,GpbDefinitionSetREF>(value);
+            return(clone.values().iterator());
+        }
+    }
+    
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2959)
+    public Map<CamelCaseName,GpbDefinitionSetREF> getMVCopy(){
+        synchronized(this){
+            Map<CamelCaseName,GpbDefinitionSetREF> clone = null;
+            if (getAttributeInfo().valueType == ValueTypeEnum.HASHMAPPED){
+                if (value == null)
+                    clone = new HashMap<CamelCaseName,GpbDefinitionSetREF>();
+                else
+                    clone = new HashMap<CamelCaseName,GpbDefinitionSetREF>(value);
+            }
+            else{
+                if (value == null)
+                    clone = new TreeMap<CamelCaseName,GpbDefinitionSetREF>();
+                else
+                    clone = new TreeMap<CamelCaseName,GpbDefinitionSetREF>(value);
+            }
+            return(clone);
+        }
+    }
+    
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2980)
+    @Override
+    public int getMVSize(){
+        synchronized(this){
+            if (value == null)
+                return(0);
+            return(value.size());
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:2992)
+    public GpbDefinitionSetREF getByKey(Object key){
+        synchronized(this){
+           if (value == null)
+               return(null);
+    
+            if (key instanceof CamelCaseName)
+                return(value.get((CamelCaseName) key));
+            else
+                throw(new IllegalStateException("Incompatible type: " + key.getClass().getName() + " passed to del():" + getName()));
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:3007)
+    public boolean contains(Object v){
+        synchronized(this){
+           if (value == null)
+               return(false);
+    
+            try {
+                GpbDefinitionSetREF val = typeCheck(v);
+                return(value.containsValue(val));
+            } catch (DmcValueException e) {
+                return(false);
+            }
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMAPType(GenUtility.java:3024)
+    public boolean containsKey(Object key){
+        synchronized(this){
+           if (value == null)
+               return(false);
+    
+           if (key instanceof CamelCaseName)
+                return(value.containsKey(key));
+            return(false);
+        }
+    }
+    
+}
+
---
+++ trunk/src/org/dmd/gpb/shared/generated/types/DmcTypeGpbTypeREFMV.java
@@ -0,0 +1,229 @@
+//	---------------------------------------------------------------------------
+//	dark-matter-data
+//	Copyright (c) 2011 dark-matter-data committers
+//	---------------------------------------------------------------------------
+//	This program is free software; you can redistribute it and/or modify it
+//	under the terms of the GNU Lesser General Public License as published by the
+//	Free Software Foundation; either version 3 of the License, or (at your
+//	option) any later version.
+//	This program is distributed in the hope that it will be useful, but WITHOUT
+//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+//	more details.
+//	You should have received a copy of the GNU Lesser General Public License along
+//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	---------------------------------------------------------------------------
+package org.dmd.gpb.shared.generated.types;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import org.dmd.dmc.DmcAttribute;
+import org.dmd.dmc.DmcAttributeInfo;
+import org.dmd.dmc.DmcValueException;
+/**
+ * The DmcTypeGpbTypeREFMV provides storage for a multi-valued GpbTypeREF
+ * <P>
+ * This code was auto-generated and shouldn't be altered manually!
+ * Generated from: org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2299)
+ *    Called from: org.dmd.dms.util.DmoTypeFormatter.dumpNamedREF(DmoTypeFormatter.java:531)
+ */
+@SuppressWarnings("serial")
+public class DmcTypeGpbTypeREFMV extends DmcTypeGpbTypeREF implements Serializable {
+    
+    private final static Iterator<GpbTypeREF> emptyList = (new ArrayList<GpbTypeREF>()).iterator();
+    
+    protected ArrayList<GpbTypeREF> value;
+    
+    public DmcTypeGpbTypeREFMV(){
+    
+    }
+    
+    public DmcTypeGpbTypeREFMV(DmcAttributeInfo ai){
+        super(ai);
+    }
+    
+    @Override
+    public DmcTypeGpbTypeREFMV getNew(){
+        return(new DmcTypeGpbTypeREFMV(getAttributeInfo()));
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2330)
+    public DmcAttribute<GpbTypeREF> cloneIt(){
+        synchronized(this){
+            DmcTypeGpbTypeREFMV rc = getNew();
+    
+            if (value == null)
+                return(rc);
+    
+            if (getAttributeInfo().indexSize == 0){
+                for(GpbTypeREF val: value)
+                try {
+                    rc.add(val);
+                } catch (DmcValueException e) {
+                    throw(new IllegalStateException("typeCheck() should never fail here!",e));
+                }
+            }
+            else{
+                for(int index=0; index<value.size(); index++)
+                    try {
+                        rc.setMVnth(index, value.get(index));
+                    } catch (DmcValueException e) {
+                        throw(new IllegalStateException("typeCheck() should never fail here!",e));
+                    }
+            }
+            return(rc);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2363)
+    public GpbTypeREF add(Object v) throws DmcValueException {
+        synchronized(this){
+            GpbTypeREF rc = typeCheck(v);
+            if (value == null)
+                value = new ArrayList<GpbTypeREF>();
+            value.add(rc);
+            return(rc);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2376)
+    public GpbTypeREF del(Object v){
+        synchronized(this){
+            if (value == null)
+                return(null);
+    
+            GpbTypeREF key = null;
+            GpbTypeREF rc = null;
+            try {
+                key = typeCheck(v);
+            } catch (DmcValueException e) {
+                throw(new IllegalStateException("Incompatible type passed to del():" + getName(),e));
+            }
+            int indexof = value.indexOf(key);
+            if (indexof != -1){
+                rc = value.get(indexof);
+                value.remove(rc);
+            }
+
+            return(rc);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2410)
+    public Iterator<GpbTypeREF> getMV(){
+        synchronized(this){
+            if (value == null)
+                return(emptyList);
+    
+            ArrayList<GpbTypeREF> clone = new ArrayList<GpbTypeREF>(value);
+            return(clone.iterator());
+        }
+    }
+    
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2422)
+    public ArrayList<GpbTypeREF> getMVCopy(){
+        synchronized(this){
+            if (value == null)
+                return(new ArrayList<GpbTypeREF>());
+            else 
+                return(new ArrayList<GpbTypeREF>(value));
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2434)
+    public int getMVSize(){
+        synchronized(this){
+            if (getAttributeInfo().indexSize == 0){
+                if (value == null)
+                    return(0);
+                return(value.size());
+            }
+            else
+                return(getAttributeInfo().indexSize);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2449)
+    public GpbTypeREF getMVnth(int index){
+        synchronized(this){
+            if (value == null)
+                return(null);
+            return(value.get(index));
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2460)
+    public GpbTypeREF setMVnth(int index, Object v) throws DmcValueException {
+        synchronized(this){
+            if (getAttributeInfo().indexSize == 0)
+                throw(new IllegalStateException("Attribute: " + getAttributeInfo().name + " is not indexed. You can't use setMVnth()."));
+            
+            if ( (index < 0) || (index >= getAttributeInfo().indexSize))
+                throw(new IllegalStateException("Index " + index + " for attribute: " + getAttributeInfo().name + " is out of range: 0 <= index < " + getAttributeInfo().indexSize));
+            
+            GpbTypeREF rc = null;
+            
+            if (v != null)
+                rc = typeCheck(v);
+            
+            if (value == null){
+                value = new ArrayList<GpbTypeREF>(getAttributeInfo().indexSize);
+                for(int i=0;i<getAttributeInfo().indexSize;i++)
+                    value.add(null);
+            }
+            
+            value.set(index, rc);
+            
+            return(rc);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2488)
+    public boolean hasValue(){
+        synchronized(this){
+            boolean rc = false;
+            
+            if (getAttributeInfo().indexSize == 0)
+                throw(new IllegalStateException("Attribute: " + getAttributeInfo().name + " is not indexed. You can't use hasValue()."));
+            
+            if (value == null)
+                return(rc);
+            
+            for(int i=0; i<value.size(); i++){
+                if (value.get(i) != null){
+                    rc = true;
+                    break;
+                }
+            }
+            
+            return(rc);
+        }
+    }
+    
+    @Override
+    // org.dmd.dms.util.GenUtility.dumpMVType(GenUtility.java:2512)
+    public boolean contains(Object v){
+        synchronized(this){
+            if (value == null)
+                return(false);
+
+            try {
+                GpbTypeREF val = typeCheck(v);
+                return(value.contains(val));
+            } catch (DmcValueException e) {
+                return(false);
+            }
+        }
+    }
+    
+}
+
