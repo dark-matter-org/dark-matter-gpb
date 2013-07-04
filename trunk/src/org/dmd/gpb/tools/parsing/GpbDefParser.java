3bd3b327bdf8af1589f2878080572ccc71bc5e4c
pstrong99@gmail.com <pstrong99@gmail.com@f9b6f35e-91de-e0b8-5825-bcd424a19d09>
2013-07-04 17:44:35 +0000

    Initial work on parser

--- trunk/src/org/dmd/gpb/tools/parsing/GpbDefParser.java
+++ trunk/src/org/dmd/gpb/tools/parsing/GpbDefParser.java
@@ -1,10 +1,18 @@
 package org.dmd.gpb.tools.parsing;
 
 import org.dmd.dmc.DmcValueException;
+import org.dmd.dmc.rules.DmcRuleExceptionSet;
+import org.dmd.dmc.util.DmcUncheckedObject;
+import org.dmd.dms.AttributeDefinition;
+import org.dmd.dms.MetaSchemaAG;
 import org.dmd.dms.SchemaManager;
 import org.dmd.gpb.server.generated.DmdgpbSchemaAG;
 import org.dmd.util.exceptions.ResultException;
 import org.dmd.util.parsing.ConfigFinder;
+import org.dmd.util.parsing.ConfigLocation;
+import org.dmd.util.parsing.ConfigVersion;
+import org.dmd.util.parsing.DmcUncheckedOIFHandlerIF;
+import org.dmd.util.parsing.DmcUncheckedOIFParser;
 
 /**
  * The GpbDefParser parses dark-matter Google Protocol Buffer specifications from files ending
@@ -12,13 +20,17 @@ import org.dmd.util.parsing.ConfigFinder;
  * @author peter
  *
  */
-public class GpbDefParser {
+public class GpbDefParser implements DmcUncheckedOIFHandlerIF {
 	
 	SchemaManager	schema;
 	
 	ConfigFinder	finder;
 	
-	public GpbDefParser(ConfigFinder cf) throws ResultException, DmcValueException {
+    // The parser that will read basic objects from our information model
+    // definition files that contain the other types of definitions.
+    DmcUncheckedOIFParser       defParser;
+
+    public GpbDefParser(ConfigFinder cf) throws ResultException, DmcValueException {
 		schema = new SchemaManager();
 		
 		DmdgpbSchemaAG s = new DmdgpbSchemaAG();
@@ -26,9 +38,38 @@ public class GpbDefParser {
 		schema.manageSchema(s);
 		
 		finder = cf;
+		
+		defParser = new DmcUncheckedOIFParser(this);
+		
+        for (AttributeDefinition def: MetaSchemaAG._metaSchema.getAttributeDefList()){
+        	if (def.getPreserveNewlines()){
+        		defParser.addPreserveNewlinesAttribute(def.getName().getNameString());
+        	}
+        }
+
 	}
 	
-	public void parse(String configName){
+	public void parse(String configName) throws ResultException, DmcValueException, DmcRuleExceptionSet {
+    	ConfigVersion		config		= finder.getConfig(configName);
+    	ConfigLocation		location	= null;
+        String          	currFile 	= null;
+
+        if (config == null){
+        	ResultException ex = new ResultException();
+        	ex.addError("The specified dark-matter GPB spec couldn't be found: " + configName);
+        	throw(ex);
+        }
+        
+        location = config.getLatestVersion();
+        currFile = location.getFileName();
+
+    	defParser.parseFile(currFile, location.isFromJAR());
+
+	}
+
+	@Override
+	public void handleObject(DmcUncheckedObject obj, String infile, int lineNumber) throws ResultException, DmcValueException, DmcRuleExceptionSet {
+		System.out.println(obj.toOIF());
 		
 	}
 
