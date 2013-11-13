cd28cfe471debfcf7b93e03d9f9cb100df3f5a50
pstrong99 <pstrong99@f9b6f35e-91de-e0b8-5825-bcd424a19d09>
2013-11-13 04:42:02 +0000

    Initial generation of definition manager and definition parser

---
+++ trunk/src/org/dmd/gpb/server/generated/dsd/GpbModuleDefinitionManager.java
@@ -0,0 +1,50 @@
+//	---------------------------------------------------------------------------
+//	dark-matter-data
+//	Copyright (c) 2013 dark-matter-data committers
+//	---------------------------------------------------------------------------
+//	This program is free software; you can redistribute it and/or modify it
+//	under the terms of the GNU Lesser General Public License as published by the
+//	Free Software Foundation; either version 3 of the License, or (at your
+//	option) any later version.
+//	This program is distributed in the hope that it will be useful, but WITHOUT
+//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+//	more details.
+//	You should have received a copy of the GNU Lesser General Public License along
+//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	---------------------------------------------------------------------------
+package org.dmd.gpb.server.generated.dsd;
+
+// Generated from: org.dmd.util.codegen.ImportManager.getFormattedImports(ImportManager.java:82)
+// Called from: org.dmd.dmg.generators.DSDArtifactFormatter.generateDefinitionManager(DSDArtifactFormatter.java:94)
+import org.dmd.dmc.definitions.DmcDefinitionSet;          // Our base to provide definition set storage - (DSDArtifactFormatter.java:92)
+import org.dmd.gpb.server.extended.GpbArtifact;           // A definition from the GpbModule Module - (DSDArtifactFormatter.java:120)
+import org.dmd.gpb.server.extended.GpbDefinition;         // A definition from the GpbModule Module - (DSDArtifactFormatter.java:117)
+import org.dmd.gpb.server.extended.GpbEnum;               // A definition from the GpbModule Module - (DSDArtifactFormatter.java:120)
+import org.dmd.gpb.server.extended.GpbField;              // A definition from the GpbModule Module - (DSDArtifactFormatter.java:120)
+import org.dmd.gpb.server.extended.GpbMessage;            // A definition from the GpbModule Module - (DSDArtifactFormatter.java:120)
+import org.dmd.gpb.server.extended.GpbType;               // A definition from the GpbModule Module - (DSDArtifactFormatter.java:120)
+
+// Generated from: org.dmd.dmg.generators.DSDArtifactFormatter.generateDefinitionManager(DSDArtifactFormatter.java:98)
+public class GpbModuleDefinitionManager {
+
+    DmcDefinitionSet<GpbDefinition> GpbDefinitionDefs;
+    DmcDefinitionSet<GpbType> GpbTypeDefs;
+    DmcDefinitionSet<GpbEnum> GpbEnumDefs;
+    DmcDefinitionSet<GpbField> GpbFieldDefs;
+    DmcDefinitionSet<GpbMessage> GpbMessageDefs;
+    DmcDefinitionSet<GpbArtifact> GpbArtifactDefs;
+
+    GpbModuleDefinitionManager(){
+
+        GpbDefinitionDefs = new DmcDefinitionSet<GpbDefinition>();
+        GpbTypeDefs = new DmcDefinitionSet<GpbType>();
+        GpbEnumDefs = new DmcDefinitionSet<GpbEnum>();
+        GpbFieldDefs = new DmcDefinitionSet<GpbField>();
+        GpbMessageDefs = new DmcDefinitionSet<GpbMessage>();
+        GpbArtifactDefs = new DmcDefinitionSet<GpbArtifact>();
+
+    }
+
+}
+
