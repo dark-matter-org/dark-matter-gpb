534eb620ce0d3de347cdfa7a922d833d59669251
pstrong99 <pstrong99@f9b6f35e-91de-e0b8-5825-bcd424a19d09>
2013-11-28 04:57:10 +0000

    Starting on the parser

---
+++ trunk/src/org/dmd/gpb/tools/parsing/GpbDotProtoParser.java
@@ -0,0 +1,12 @@
+package org.dmd.gpb.tools.parsing;
+
+/**
+ * The GpbDotProtoParser is used to parse existing Google Protocol Buffer .proto
+ * files and generate the dark-matter GPB (dmgpb) equivalent specification that can then
+ * be augmented with documentation.
+ * <p>
+ * This approach allows for a quickstart usage of the extended concepts of dmgpb.
+ */
+public class GpbDotProtoParser {
+
+}
