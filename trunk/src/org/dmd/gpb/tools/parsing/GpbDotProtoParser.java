e1e13f195f0f0c264042ba654a9f35602acf0c90
pstrong99 <pstrong99@f9b6f35e-91de-e0b8-5825-bcd424a19d09>
2013-11-28 22:06:05 +0000

    Working on proto parser

--- trunk/src/org/dmd/gpb/tools/parsing/GpbDotProtoParser.java
+++ trunk/src/org/dmd/gpb/tools/parsing/GpbDotProtoParser.java
@@ -1,5 +1,13 @@
 package org.dmd.gpb.tools.parsing;
 
+import java.io.IOException;
+
+import org.dmd.util.exceptions.ResultException;
+import org.dmd.util.parsing.ConfigFinder;
+import org.dmd.util.parsing.ConfigLocation;
+import org.dmd.util.parsing.ConfigVersion;
+import org.dmd.util.parsing.StringArrayList;
+
 /**
  * The GpbDotProtoParser is used to parse existing Google Protocol Buffer .proto
  * files and generate the dark-matter GPB (dmgpb) equivalent specification that can then
@@ -8,5 +16,32 @@ package org.dmd.gpb.tools.parsing;
  * This approach allows for a quickstart usage of the extended concepts of dmgpb.
  */
 public class GpbDotProtoParser {
+	
+	ConfigFinder	finder;			// Our .proto file finder
+	
+	/**
+	 * Constructs a new .proto parser.
+	 * @param sd the source directories to search for .proto files.
+	 */
+	public GpbDotProtoParser(StringArrayList sd){
+		finder = new ConfigFinder("proto",sd, false);
+	}
+	
+	public void run() throws ResultException, IOException {
+		finder.findConfigs();
+		
+	}
+	
+	public void debug(boolean f){
+		finder.debug(f);
+	}
 
+	public ConfigLocation getConfig(String cn){
+		ConfigVersion cv = finder.getConfig(cn);
+		
+		if (cv == null)
+			return(null);
+		
+		return(cv.getLatestVersion());
+	}
 }
---
+++ trunk/test/org/dmd/gpb/tools/parsing/GpbDotProtoParserTest.java
@@ -0,0 +1,35 @@
+package org.dmd.gpb.tools.parsing;
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.dmd.util.exceptions.ResultException;
+import org.dmd.util.parsing.StringArrayList;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GpbDotProtoParserTest {
+
+	static String workingDir;
+
+	@Before
+	public void initialize() throws IOException {
+        File curr = new File(".");
+        workingDir = curr.getCanonicalPath();
+		System.out.println("*** Generator running from: " + workingDir);
+	}
+
+	@Test
+	public void testBasicFind() throws ResultException, IOException {
+		StringArrayList sd = new StringArrayList();
+		sd.add(workingDir + "/test/org/dmd/gpb/tools/parsing");
+		GpbDotProtoParser parser = new GpbDotProtoParser(sd);
+		parser.debug(true);
+		
+		parser.run();
+		
+		Assert.assertNotNull(parser.getConfig("config1"));
+	}
+}
